f62e040d7cf9c0d377036c154519812214df78ee
Stjender <426638@student.fontys.nl>
2020-06-04 14:51:02 +0200

    na de resolutie fix

--- Assets/Scripts/ObjectCheck.cs
+++ Assets/Scripts/ObjectCheck.cs
@@ -1,9 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using TMPro;
-using UnityEditor;
-using UnityEditor.Experimental.UIElements;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ObjectCheck : MonoBehaviour
 {
