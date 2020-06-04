2bf42335bd646f53028e36e6d27cd660da5a9356
Emre Dogan <47520970+tehnumberone@users.noreply.github.com>
2020-06-04 15:21:11 +0200

    t2

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -27,8 +27,6 @@ public class Item : MonoBehaviour
     [HideInInspector]
     public GameObject EquipmentManager;
 
-    private ItemUse itemUse;
-
     public void Start()
     {
         EquipmentManager = GameObject.FindWithTag("EquipManager");
@@ -47,16 +45,9 @@ public class Item : MonoBehaviour
         equipped = true;
         gameObject.SetActive(false);
     }
-
-    public void OnUse(GameObject Object)
+    
+    public void OnUse()
     {
-        if (this.itemType == ItemTypes.Tool)
-        {
-            itemUse.UseTool(this, Object);
-        }
-        if (this.itemType == ItemTypes.Food)
-        {
-            itemUse.UseFood(this);
-        }
+
     }
 }
