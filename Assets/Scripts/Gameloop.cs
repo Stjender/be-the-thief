a20abb9280e4f9ce8a58158cc6e6eaf0ae1f879f
Stjender <426638@student.fontys.nl>
2020-06-25 15:29:42 +0200

    scene naam verandering en gameloop

---
+++ Assets/Scripts/Gameloop.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Gameloop : MonoBehaviour
+{
+    private int currentLevel = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetupLevel1();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void NextLevel()
+    {
+        currentLevel++;
+        switch (currentLevel)
+        {
+            case 2:
+                SetupLevel2();
+                break;
+
+
+            default:
+                break;
+        }
+    }
+
+    void ResetScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    void SetupLevel1()
+    {
+
+    }
+
+    void SetupLevel2()
+    {
+
+    }
+}
